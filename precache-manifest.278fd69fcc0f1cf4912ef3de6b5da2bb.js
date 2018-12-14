3eb457f1431e28dc3199a8688bf23333e33823b4
Kevin Davis <dawnpatrol506@gmail.com>
2018-12-14 10:22:56 -0700

    Updates

---
+++ precache-manifest.278fd69fcc0f1cf4912ef3de6b5da2bb.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "6acb6692e4ee25941da6",
+    "url": "/clicky-game/static/js/runtime~main.6acb6692.js"
+  },
+  {
+    "revision": "e5c0008a6f064a69d10a",
+    "url": "/clicky-game/static/js/main.e5c0008a.chunk.js"
+  },
+  {
+    "revision": "324b6cf3b56bf43378ce",
+    "url": "/clicky-game/static/js/1.324b6cf3.chunk.js"
+  },
+  {
+    "revision": "fef959c9d0c1e4fa523173d782eb01d3",
+    "url": "/clicky-game/index.html"
+  }
+];
